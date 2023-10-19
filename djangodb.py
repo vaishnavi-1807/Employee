74475c9f492103c5b47a3358955e1e36769bb551
vaishnavi-1807 <148449348+vaishnavi-1807@users.noreply.github.com>
2023-10-19 16:43:57 +0530

    Create djangodb.py

---
+++ djangodb.py
@@ -0,0 +1,10 @@
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'djangodb',  
+        'USER':'root',  
+        'PASSWORD':'mysql',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
+    }  
+}  
